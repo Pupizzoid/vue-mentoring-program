36a1d4564fa4a46b928f630ff3cf25dd53df9f71
Vita  Kozlovska <vitakozlovska5@gmail.com>
2023-10-02 16:35:51 +0300

    added new configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,22 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+export default {
   testEnvironment: 'jsdom',
+  preset: 'ts-jest',
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\js$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!pinia/.*)',
+    '<rootDir>/node_modules/(?!@pinia/testing)',
+    '<rootDir>/node_modules/(?!vue-demi/.*)',
+    '<rootDir>/node_modules/(?!@vue/test-utils)',
+  ],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
-  moduleFileExtensions: ['vue', 'js'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
   coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
   coverageReporters: ['text', 'json-summary'],
   testEnvironmentOptions: {
     customExportConditions: ['node', 'node-addons'],
   },
 };
-
-export default config;
